6092f0f1ad107e24332fb232de87790a4c815108
erniseptia <92901309+erniseptia@users.noreply.github.com>
2022-11-05 14:55:53 +0700

    data kariyawan

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component }  from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from './navigasi/Home.js';
+import Detail from './navigasi/Detail.js';
+
+
+const Stack = createNativeStackNavigator();
+function App () {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}
+        option={{
+          title:'Data Karyawan',
+          headerStyle: {backgroundColor:'#006aff'},
+          headerTintColor:'#fff'
+        }}/>
+                <Stack.Screen name="Detail" component={Detail}
+        option={{
+          title:'Data Karyawan',
+          headerStyle: {backgroundColor:'#006aff'},
+          headerTintColor:'#fff'
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); 
+}
+
+export default App;
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# DataKaryawan
+# DataKaryawan
+# DataKaryawan
+# DataKaryawan
