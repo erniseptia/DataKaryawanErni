e0145c2e73625e61147fe1a01aa83ec4a01e3e54
erniseptia <92901309+erniseptia@users.noreply.github.com>
2022-11-14 15:18:17 +0700

    Katalog-Produk

--- App.js
+++ App.js
@@ -1,32 +1,65 @@
-import React, { Component }  from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from './navigasi/Home.js';
-import Detail from './navigasi/Detail.js';
+import React, { useState, useEffect }  from "react";
+import { Text, View, FlatList, ImageBackground} from "react-native";
+import { Card } from "react-native-paper";
+import styles from "./styles";
 
+export default function App() {
+  const [data, setData] = useState([]);
 
-const Stack = createNativeStackNavigator();
-function App () {
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/algosigma/js-reactjs/master/homestays.json')
+    .then((response)=> response.json())
+    .then((hasil)=> setData(hasil))
+    .catch(error=>{console.log;})
+  }, []);
+
+  const listHomestay = ({item}) => {
+    return (
+      <Card style={styles.cardUtama}>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>{item.nama}</Text>
+        </View>
+        <View style={styles.containerImage}>
+          <ImageBackground style={styles.image}
+          source={{uri:item.fotoUrl}}>
+            <Text style={styles.price}>Rp. {item.harga} Rb</Text>
+          </ImageBackground>
+        </View>
+      </Card>
+    )
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}
-        option={{
-          title:'Data Karyawan',
-          headerStyle: {backgroundColor:'#006aff'},
-          headerTintColor:'#fff'
-        }}/>
-                <Stack.Screen name="Detail" component={Detail}
-        option={{
-          title:'Data Karyawan',
-          headerStyle: {backgroundColor:'#006aff'},
-          headerTintColor:'#fff'
-        }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  ); 
+    <View style={{flex:1, padding:24}}>
+      <FlatList
+      data={data}
+      keyExtractor={item=>item.id}
+      renderItem={listHomestay}/>
+    </View>
+  );
 }
 
-export default App;
+// const Stack = createNativeStackNavigator();
+// function App () {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={Home}
+//         option={{
+//           title:'Data Karyawan',
+//           headerStyle: {backgroundColor:'#006aff'},
+//           headerTintColor:'#fff'
+//         }}/>
+//                 <Stack.Screen name="Detail" component={Detail}
+//         option={{
+//           title:'Data Karyawan',
+//           headerStyle: {backgroundColor:'#006aff'},
+//           headerTintColor:'#fff'
+//         }}/>
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   ); 
+// }
+
+// export default App;
 
